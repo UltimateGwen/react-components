b0d2b0dd49c053701213cb71f07107ddfc2621f0
Danya <danyafrisina@hotmail.it>
2020-02-03 10:57:48 +0100

    Quest 4 React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,37 @@
 import React from "react";
 import "./Contact.css";
 
-function Contact(props) {
-  const { name, avatar, online } = props;
+/*function Contact(props)*/
 
-  return (
-    <div className="Contact">
-      <img className="avatar" src={avatar} alt="" />
-      <div>
-        <h4 className="name">{name}</h4>
-        <p className="status">{online ? "Online" : "Offline"}</p>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: this.props.online
+    };
+  }
+
+  /*const { name, avatar, online } = props;*/
+
+  changeStatus = e => {
+    e.preventDefault();
+
+    this.setState({ online: !this.state.online });
+  };
+
+  render() {
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} alt="" />
+        <div>
+          <h4 className="name">{this.props.name}</h4>
+          <p className="status" onClick={this.changeStatus}>
+            {this.state.online ? "Online" : "Offline"}
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Contact;
